701f5085cf88f55928db2fbaf6f2c64bd17bead7
Mohit R. Dhanjani <md@iconappcom.in>
2016-12-20 00:51:15 +0530

    First commit of working module.

---
+++ AMS.js
@@ -0,0 +1,177 @@
+var cheerio = require('cheerio');
+var request = require('request');
+var Promise = require('bluebird');
+var S = require('string');
+var fs = require('fs');
+var md5 = require('md5');
+
+require('dotenv').config();
+var AMSDomain = process.env.AMS_DOMAIN
+var AMSStudentPageUrl = process.env.AMS_STUDENT_PAGE_URL
+var AMSStudentPageAlternateUrl = process.env.AMS_STUDENT_PAGE_URL_ALTERNATE;
+
+var parseAttendance = function(data) {
+	
+	return new Promise(function(resolve, reject){
+		var attendance = [];
+		$ = cheerio.load(data);
+		var rows = $;
+		var numRows = rows.length;
+		for(i=0;i<=numRows;i++){
+			
+			$('tr').each(function(a, ele){
+				var subject = {};
+				$(this).children().each(function(n, element){
+                    if(n === 0) {
+                        subject.code = $(this).find('span').text().replace('\n', '');
+                    }
+					if(n === 1){
+						subject.name = $(this).find('span').text().replace('\n', '');
+					}
+					if(n === 5){
+						subject.attendance = parseInt($(this).find('span').text());
+						attendance[a] = subject;
+						subject = null;
+					}
+					
+				});
+			});
+		}
+		resolve(attendance);
+	});
+}
+
+var parseMarks = function(data) {
+
+
+    return new Promise(function(resolve, reject){
+		var marks = [];
+		$ = cheerio.load(data);
+		var rows = $;
+		var numRows = rows.length;
+		for(i=0;i<=numRows;i++){
+			
+			$('tr').each(function(a, ele){
+				var subject = {};
+				$(this).children().each(function(n, element){
+                    if(n === 0) {
+                        subject.code = $(this).find('span').text().replace('\n', '');
+                    }
+					if(n === 1){
+						subject.name = $(this).find('span').text().replace('\n', '');
+					}
+					if(n === 2){
+						subject.marks = parseFloat($(this).find('span').text());
+						marks[a] = subject;
+						subject = null;
+					}
+					
+				});
+			});
+		}
+		resolve(marks);
+	});
+}
+
+var parseGPA = function(data) {
+    return new Promise(function(resolve, reject){
+		var gpa = {};
+		$ = cheerio.load(data);
+		var rows = $;
+		var numRows = rows.length;
+			
+			$('tr').each(function(a, ele){
+				var subject = {};
+                var seminfo;
+				$(this).children().each(function(n, element){
+					if(n === 0){
+                        seminfo = $(this).find('span').text();
+                        seminfo = seminfo.charAt(seminfo.length-1);
+					}
+					if(n === 1){
+                        gpa[seminfo]  = parseFloat($(this).find('span').text());
+                        if(isNaN(gpa[seminfo])){
+                            gpa[seminfo] = null;
+                        }
+					}
+					
+				});
+			});
+		resolve(gpa);
+	});
+}
+
+module.exports.getAllData = function(userID, type, username, password){
+
+    var form = {USERNAME:username, PASSWORD:password};
+
+    if(type === "regbirth") {
+        AMSStudentPageUrl = AMSStudentPageAlternateUrl;
+        form.idValue = username;
+        form.birthDate_i18n = password;
+        form.birthDate = password;
+        delete form.USERNAME;
+        delete form.PASSWORD;
+    }
+
+    var dataForDB = {};
+    dataForDB.marks = [];
+
+    return new Promise(function(resolve, reject){
+
+        var takeSecondAction = function(err, response, body) {
+                    
+                    if (!err && response.statusCode == 200) {
+
+                        dataForDB.md5value = md5(body);
+                    
+                        $ = cheerio.load(body);
+
+                        
+                        $('table').each(function(i, ele){
+                            if(ele.attribs.id === 'ListAttendanceSummary_table') {
+                                
+                                $('#ListAttendanceSummary_table > script').remove();
+                                $('#ListAttendanceSummary_table > thead').remove();
+                                parseAttendance($('#ListAttendanceSummary_table').html()).then(function(data){
+                                    dataForDB.attendance = data;
+                                });
+                            }
+                            else if(ele.attribs.id === "ListAssessmentScores_table") {
+                                $(this).children('thead').remove();
+                                $(this).children('script').remove();
+                                parseMarks(ele).then(function(res){
+                                    dataForDB.marks.push(res);
+                                });
+                            }
+                            else if(ele.attribs.id === "ProgramAdmissionItemSummary_table") {
+                                $(this).children('script').remove();
+                                parseGPA(ele).then(function(res){
+                                    dataForDB.gpa = res;
+                                });
+                            }
+                        });
+
+                        return resolve(dataForDB);
+                        
+                        
+                        }
+        }
+
+        var takeAction = function(err, response, body){
+            if(response.statusCode == 200) {
+                $ = cheerio.load(body);
+                
+                var link = $('a:contains("Academic Status")').attr('href');
+                
+                var cookie = response.headers['set-cookie'];
+
+                var options = {url:AMSDomain + link, headers: {'Cookie': cookie}};
+                request(options, takeSecondAction);
+            }
+        };
+
+        request.post({url:AMSDomain + AMSStudentPageUrl, form:form}, takeAction);
+
+    });
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+var request = require('request');
+var cheerio = require('cheerio');
+const Consumer = require('sqs-consumer'); 
+var Promise = require('bluebird');
+var AWS = require('aws-sdk');
+var AMS = require('./AMS');
+var Dynamite = require('dynamite');
+
+require('dotenv').config();
+
+const DDBTable = process.env.DYNAMODB_TABLE;
+const DDBHashKey = process.env.DYNAMODB_HASH_KEY;
+const QueueUrl = process.env.SQS_QUEUE_URL;
+
+var dbclient = new Dynamite.Client({region: process.env.AWS_REGION});
+
+var putDataInDB = function(userID, data) {
+
+    return new Promise(function(resolve, reject){
+        dbclient.newUpdateBuilder(DDBTable)
+        .setHashKey(DDBHashKey, userID)
+        .enableUpsert()
+        .putAttribute('latest_sem', JSON.stringify(data))
+        .putAttribute('gpa', JSON.stringify(data.gpa))
+        .putAttribute('md5_hash', data.md5value)
+        .execute().then(function(ds){
+            resolve(true);
+        }).fail(function(err){
+            console.log(err);
+        });
+    });
+
+    
+}
+
+exports.handler = function(event, context) {
+   
+   const app = Consumer.create({
+        queueUrl: QueueUrl,
+        handleMessage: (message, done) => {
+            var userData = JSON.parse(message.Body);
+            console.log(userData);
+            AMS.getAllData(userData.userID, userData.type, userData.username, userData.password).then(function(data){
+                putDataInDB(userData.userID, data).then(() => done());
+            });
+        },
+        sqs : new AWS.SQS()
+    });
+
+    app.on('error', (err) => {
+        console.log("Eroor in queue - " + err.message);
+    });
+
+    app.on('empty', () => {
+       context.succeed("The queue is empty. Quiting process");
+    });
+
+    app.start();
+    
+}
+
+//Uncomment below line for testing fast.
+//require('make-runnable');
